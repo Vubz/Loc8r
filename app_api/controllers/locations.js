0e75efca00541bf9a9f328bfd43e156ddae949fd
vubz <vubrionesz@gmail.com>
2018-02-05 00:51:52 -0800

    Api server and rendering from api

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,17 +1,109 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
+var theEarth = (function () {
+  var earthRadious = 6371; //radious in km
+  var getDistanceFromRads = function (rads) {
+    return parseFloat(rads*earthRadious);
+  };
+  var getRadsFromDistance = function (distance) {
+    return parseFloat(distance/earthRadious);
+  };
+  return {
+    getDistanceFromRads : getDistanceFromRads,
+    getRadsFromDistance : getRadsFromDistance
+  };
+})();
+
+var meterConversion = (function () {
+  var mToKm = function (distance) {
+    return parseFloat(distance / 1000);
+  }
+  var kmToM = function (distance) {
+    return parseFloat(distance * 1000);
+  }
+  return {
+    mToKm : mToKm,
+    kmToM : kmToM
+  };
+})();
+
 var sendJsonResponse = function (res, status, content) {
   res.status(status);
   res.json(content)
 };
 
 module.exports.locationsCreate = function (req,res) {
-  sendJsonResponse(res,200,{"status":"success"});
+  Loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+    openingTimes: [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1
+    },
+    {
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2
+    }]
+  }, function (err, location) {
+    if (err){
+      sendJsonResponse(res,400,err);
+    } else {
+      sendJsonResponse(res,201,location)
+    }
+  });
 };
 
+//Coordinates
 module.exports.locationsListByDistance = function (req,res) {
-  sendJsonResponse(res,200,{"status":"success"});
+  var lng= parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+  var maxDistance = 20;
+  if (req.query.maxDistance){
+    maxDistance = req.query.maxDistance
+  }
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+  var geoOptions = {
+    spherical: true,
+    //maxDistance : theEarth.getRadsFromDistance(maxDistance),
+    maxDistance : meterConversion.kmToM(maxDistance),
+    num : 10
+  };
+  if ((!lng && lng!== 0) || (!lat && lat!==0)){
+    sendJsonResponse(res, 404, {
+      "message" : "lng and lat query parameter are required"
+    });
+    return;
+  };
+  Loc.geoNear(point,geoOptions, function (err, results, stats) {
+    var locations = [];
+    if (err){
+      sendJsonResponse(res, 404, err)
+      return;
+    } else {
+      results.forEach(function (doc) {
+        locations.push({
+          //distance: theEarth.getDistanceFromRads(doc.dis),
+          distance: meterConversion.mToKm(doc.dis),
+          name: doc.obj.name,
+          address: doc.obj.address,
+          rating: doc.obj.rating,
+          facilities: doc.obj.facilities,
+          _id: doc.obj._id
+        });
+      });
+      sendJsonResponse(res, 200, locations);
+    }
+  });
 };
 
 module.exports.locationsReadOne = function (req,res) {
@@ -25,7 +117,7 @@ module.exports.locationsReadOne = function (req,res) {
         sendJsonResponse(res,404,err);
         return;
       }
-      sendJsonResponse(res,200,{"status":"success"});
+      sendJsonResponse(res,200,location);
     });
   }
   else {
@@ -34,9 +126,56 @@ module.exports.locationsReadOne = function (req,res) {
 };
 
 module.exports.locationsUpdateOne = function (req,res) {
-  sendJsonResponse(res,200,{"status":"success"});
+
+  if (!req.params.locationid){
+    sendJsonResponse(res,404, {"message": "Not found, locationid is required"});
+    return;
+  }
+  Loc.findById(req.params.locationid).selecc('-reviews -rating').exec(function (err, location) {
+    if (!location){
+      sendJsonResponse(res, 404, {"message": "locationid not found"});
+      return;
+    } else if (err){
+      sendJsonResponse(res,400, err);
+      return;
+    }
+    location.name = req.body.name;
+    location.address = req.body.address;
+    location.facilities = req.body.facilities.split(",");
+    location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+    location.openingTimes = [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1
+    },{
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2
+    }];
+    location.save(function (err, location) {
+      if (err){
+        sendJsonResponse(res, 400, err);
+      } else {
+        sendJsonResponse(res, 200, location);
+      }
+    });
+  });
 };
 
 module.exports.locationsDeleteOne = function (req,res) {
-  sendJsonResponse(res,200,{"status":"success"});
+  var locationid = req.params.locationid;
+  if (locationid){
+    Loc.findByIdAndRemove(locationid).exec(function (err, location) {
+      if (err){
+        sendJsonResponse(res, 400, err);
+        return;
+      }
+      sendJsonResponse(res, 204, null);
+    })
+  } else {
+    sendJsonResponse(res, 404, {"message": "locationid not found"});
+  }
+
 };
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,40 +1,151 @@
-/* Get 'home' page */
-module.exports.homelist=function (req,res) {
+var request = require('request');
+
+var apiOptions = {
+  server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production'){
+  apiOptions.server = "https://gentle-tundra-23355.herokuapp.com";
+  //https://gentle-tundra-23355.herokuapp.com
+  //https://getting-mean-loc8r.herokuapp.com
+}
+
+/* Home Page*/
+var renderHomepage = function (req, res, responseBody) {
+  var message;
+  if (!(responseBody instanceof Array)){
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length){
+      message = "No places found nearby";
+    }
+  }
   res.render('locations-list',{
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
       title: 'Loc8r',
-      sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee.',
       strapline: 'Find places to work with wifi near you!',
-      locations: [{
-          name: 'Starcups',
-          address: '125 High Street, Reading, RG6 1PS',
-          rating: 3,
-          facilities: ['Hot drinks','Food','Premium wifi'],
-          distance: '100m'
-      },{
-          name: 'Cafe Hero',
-          address: '125 High Street, Reading, RG6 1PS',
-          rating: 4,
-          facilities: ['Hot drinks','Food','Premium wifi'],
-          distance: '150m'
-      },{
-          name: 'Burger Queen',
-          address: '125 High Street, Reading, RG6 1PS',
-          rating: 2,
-          facilities: ['Hot drinks','Food','Premium wifi'],
-          distance: '200m'
-      }]
+      sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee.',
+      locations: responseBody
+    },
+    message: message
+  });
+}
+
+/* Get 'home' page */
+module.exports.homelist=function (req,res) {
+  var requestOptions, path;
+  path = '/api/locations';
+  requestOptions = {
+    url: apiOptions.server + path,
+    method : "GET",
+    json : {},
+    qs : {
+      lng: -96.840637,
+      lat: 51.462820,
+      //lng: 0,
+      //lat: 0,
+      maxDistance: 25
     }
+  };
+  request(requestOptions,function (err, response, body) {
+    var i, data;
+    data = body;
+    if (response.statusCode === 200 && data.length){
+      for (i = 0; i < data.length; i++) {
+        data[i].distance = _formatDistance(data[i].distance);
+      }
+    }
+    renderHomepage(req,res,data);
   });
 };
 
+var _formatDistance = function (distance) {
+  var numDistance, unit;
+  if (!distance){
+    message = "";
+    return message;
+  } else if (isNaN(distance)) {
+    message = "";
+    return message;
+  }
+  if (distance > 1) {
+    numDistance = parseFloat(distance).toFixed(1);
+    unit = 'km';
+  } else {
+    numDistance = parseInt(distance * 1000, 10);
+    unit = 'm';
+  }
+  return numDistance + unit;
+};
+
+var renderDetailPage = function (req, res, locDetail) {
+  res.render('location-info', {
+    title: locDetail.name,
+    pageHeader: {title: locDetail.name},
+    sidebar: {
+      context: "some info "
+    },
+    location: locDetail
+  });
+};
 /* Get 'Location Info' page */
 module.exports.locationInfo=function (req,res) {
-  res.render('location-info',{title: 'Location Info' });
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {}
+  };
+  request(
+    requestOptions,
+    function (err, response, body) {
+      var data = body;
+
+      data.coords = {
+        lng: body.coords[0],
+        lat: body.coords[1]
+      };
+      renderDetailPage(req, res, data);
+    }
+  )
+  //res.render('location-info',{title: 'Location Info' });
 };
 
 /* Get 'addReview' page */
 module.exports.addReview=function (req,res) {
   res.render('location-review-form',{title: 'Add review' });
 };
+
+
+/*
+res.render('locations-list',{
+  title: 'Loc8r - find a place to work with wifi',
+  pageHeader: {
+    title: 'Loc8r',
+    sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee.',
+    strapline: 'Find places to work with wifi near you!',
+    locations: [{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks','Food','Premium wifi'],
+        distance: '100m'
+    },{
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks','Food','Premium wifi'],
+        distance: '150m'
+    },{
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Hot drinks','Food','Premium wifi'],
+        distance: '200m'
+    }]
+  }
+});
+
+*/
